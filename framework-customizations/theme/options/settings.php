444a88cad506e292774e7876b04f0f9c3b6a335b
Ketzenkrad <ketzenkrad@gmail.com>
2018-12-20 19:06:00 +0300

    Wordpress - полная сборка

---
+++ framework-customizations/theme/options/settings.php
@@ -0,0 +1,35 @@
+<?php if (!defined('FW')) die('Forbidden');
+
+$options = array(
+    'general' => array(
+        'type' => 'tab',
+        'title' => __('General', 'law'),
+        'options' => array(
+            'footer_text' => array(
+                'type' => 'wp-editor',
+                'label' => __('Text in Footer', 'law'),
+                'desc' => __('Write text for footer', 'law'),
+                'help' => __('Some help...', 'law'),
+            ),
+            'test_img' => array(
+                'type' => 'upload',
+                'label' => __('Header Image', 'law'),
+                'desc' => __('Header Image', 'law'),
+                'help' => __('Some help...', 'law'),
+            ),
+        ),
+    ),
+    'colors' => array(
+        'type' => 'tab',
+        'title' => __('Site colors', 'law'),
+        'options' => array(
+            'header_color' => array(
+                'type' => 'color-picker',
+                'value' => '#ffffff',
+                'label' => __('Header Color', 'law'),
+                'desc' => __('Choose header color', 'law'),
+                'help' => __('Some help...', 'law'),
+            ),
+        ),
+    ),
+);
